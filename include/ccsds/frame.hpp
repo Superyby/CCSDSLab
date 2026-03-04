5af913dcffcd4cf32d58f3e0e08475dfd1d59ccd
Superby <1551405455@qq.com>
2026-03-04 14:10:56 +0800

    init

---
+++ include/ccsds/frame.hpp
@@ -0,0 +1,7 @@
+//
+// Created by jiang on 2026/3/4.
+//
+#ifndef CCSDS_FRAME_HPP
+#define CCSDS_FRAME_HPP
+
+#endif // CCSDS_FRAME_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include "ccsds/frame.hpp"
 using namespace std;
-int main() {
+int main()
+{
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
